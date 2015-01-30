a0c75dc97d8ddb4953d5e79925cb447831590a21
jssuzanne <devnull@localhost>
2015-01-30 17:17:45 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -44,10 +44,13 @@ setup(
     install_requires=requires,
     tests_require=requires + ['nose'],
     classifiers=[
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: Mozilla Public License v2',
     ],
     entry_points={
