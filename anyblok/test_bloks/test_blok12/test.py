fce77642ecf4825b9f47feabe9d161b5829eca62
jssuzanne <devnull@localhost>
2015-05-28 00:05:57 +0200

    Fix bad fix

--- anyblok/test_bloks/test_blok12/test.py
+++ anyblok/test_bloks/test_blok12/test.py
@@ -6,32 +6,8 @@
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
 from anyblok import Declarations
-from anyblok.column import Integer, String
 
 
-register = Declarations.register
-Model = Declarations.Model
-
-
-@register(Model)
-class Test:
-
-    id = Integer(primary_key=True)
-    blok = String()
-    mode = String()
-
-
-@register(Model.System)
-class Blok:
-
-    def install(self):
-        super(Blok, self).upgrade()
-        self.registry.Test.insert(blok=self.name, mode='install')
-
-    def upgrade(self):
-        super(Blok, self).upgrade()
-        self.registry.Test.insert(blok=self.name, mode='update')
-
-    def load(self):
-        super(Blok, self).load()
-        self.registry.Test.insert(blok=self.name, mode='load')
+@Declarations.register(Declarations.Core)
+class Session:
+    """Overwrite Session for unit test"""
