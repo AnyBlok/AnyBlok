4677d8e62a7bab196dd97ccf42c76df2a94019b8
Sébastien CHAZALLET <s.chazallet@gmail.com>
2015-06-19 09:43:16 +0200

    Add some doc to the method to_dict

--- anyblok/bloks/anyblok_core/core/sqlbase.py
+++ anyblok/bloks/anyblok_core/core/sqlbase.py
@@ -196,9 +196,45 @@ class SqlMixin:
     def to_dict(self, *fields):
         """ Transform a record to the dict of value
 
-        :param fields: list of fields to put in dict, if not selected fields
-            them take all
+        :param fields: list of fields to put in dict; if not selected, fields then take them all.
+            A field is either one of these:
+                * a string (which is the name of the field)
+                * a 2-tuple if the field is a relationship (name of the field, tuple of foreign model fields)
         :rtype: dict
+
+        Here are some examples:
+        >>> instance.to_dict()  # get all fields
+        {"id": 1,
+         "column1": "value 1",
+         "column2": "value 2",
+         "column3": "value 3",
+         "relation1": {"relation_pk_1": 42, "relation_pk_2": "also 42"}  # m2o or o2o : this is a dictionary
+         "relation2": [{"id": 28}, {"id": 1}, {"id": 34}]  # o2m or m2m : this is a list of dictionaries
+         }
+        >>> instance.to_dict("column1", "column2", "relation1")  # get selected fields only (without any constraints)
+        {"column1": "value 1",
+         "column2": "value 2",
+         "relation1": {"relation_pk_1": 42, "relation_pk_2": "also 42"}
+         }
+        >>> instance.to_dict("column1", "column2", (  # select fields to use in the relation related model
+            "relation1", ("relation_pk1", "name", "value")  # there is no constraints in the choice of fields
+            ))
+        {"column1": "value",
+         "column2": "value",
+         "relation1": {"relation_pk_1": 42, "name": "H2G2", "value": "42"}
+         }
+        >>> instance.to_dict("column1", "column2", (  # select relation fields recursively
+            "relation1", ("name", "value", (
+                "relation", ("a", "b", "c")
+                ))
+            ))
+        {"column1": "value",
+         "column2": "value",
+         "relation1": {"name": "H2G2", "value": "42", "relation": [
+             {"a": 10, "b": 20, "c": 30},
+             {"a": 11, "b": 22, "c": 33},
+             ]}
+         }
         """
 
         if not fields:
