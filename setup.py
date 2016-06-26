d270d87c81e51233bcb382396b4895cbdec47bb3
jssuzanne <jssuzanne@anybox.fr>
2016-06-26 22:38:34 +0200

    add doc-requirements.txt for readthedocs

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ requires = [
 ]
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.rst')) as readme:
+with open(os.path.join(here, 'README.rst'), 'r', encoding='utf-8') as readme:
     README = readme.read()
 
 setup(
