e07734225d9f4753963fffa5a666a06751404738
jssuzanne <jssuzanne@anybox.fr>
2018-09-10 12:20:49 +0200

    Use packaging

--- anyblok/version.py
+++ anyblok/version.py
@@ -5,78 +5,50 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
-from pkg_resources import SetuptoolsVersion, SetuptoolsLegacyVersion
+from packaging.version import Version, LegacyVersion
 from pkg_resources.extern import packaging
 
 
 class VersionComparator:
 
-    def __lt__(self, other):
+    def parse_other(self, other):
         if other is None:
-            other = SetuptoolsLegacyVersion('')
-        elif not isinstance(
-            other, (SetuptoolsVersion, SetuptoolsLegacyVersion)
-        ):
+            other = LegacyVersion('')
+        elif not isinstance(other, (Version, LegacyVersion)):
             other = parse_version(other)
 
+        return other
+
+    def __lt__(self, other):
+        other = self.parse_other(other)
         return super(VersionComparator, self).__lt__(other)
 
     def __le__(self, other):
-        if other is None:
-            other = SetuptoolsLegacyVersion('')
-        elif not isinstance(
-            other, (SetuptoolsVersion, SetuptoolsLegacyVersion)
-        ):
-            other = parse_version(other)
-
+        other = self.parse_other(other)
         return super(VersionComparator, self).__le__(other)
 
     def __eq__(self, other):
-        if other is None:
-            other = SetuptoolsLegacyVersion('')
-        elif not isinstance(
-            other, (SetuptoolsVersion, SetuptoolsLegacyVersion)
-        ):
-            other = parse_version(other)
-
+        other = self.parse_other(other)
         return super(VersionComparator, self).__eq__(other)
 
     def __ne__(self, other):
-        if other is None:
-            other = SetuptoolsLegacyVersion('')
-        elif not isinstance(
-            other, (SetuptoolsVersion, SetuptoolsLegacyVersion)
-        ):
-            other = parse_version(other)
-
+        other = self.parse_other(other)
         return super(VersionComparator, self).__ne__(other)
 
     def __ge__(self, other):
-        if other is None:
-            other = SetuptoolsLegacyVersion('')
-        elif not isinstance(
-            other, (SetuptoolsVersion, SetuptoolsLegacyVersion)
-        ):
-            other = parse_version(other)
-
+        other = self.parse_other(other)
         return super(VersionComparator, self).__ge__(other)
 
     def __gt__(self, other):
-        if other is None:
-            other = SetuptoolsLegacyVersion('')
-        elif not isinstance(
-            other, (SetuptoolsVersion, SetuptoolsLegacyVersion)
-        ):
-            other = parse_version(other)
-
+        other = self.parse_other(other)
         return super(VersionComparator, self).__gt__(other)
 
 
-class AnyBlokVersion(VersionComparator, SetuptoolsVersion):
+class AnyBlokVersion(VersionComparator, Version):
     pass
 
 
-class AnyBlokLegacyVersion(VersionComparator, SetuptoolsLegacyVersion):
+class AnyBlokLegacyVersion(VersionComparator, LegacyVersion):
     pass
 
 
