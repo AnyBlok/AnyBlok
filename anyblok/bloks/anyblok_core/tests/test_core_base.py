0719c7bc784026fa1edde827cb7fa86c9f0420cc
denis-vivies-home <denis-vivies@netcourrier.com>
2018-10-04 18:04:55 +0200

    add init_session&rollback_registry in conftest

--- anyblok/bloks/anyblok_core/tests/test_core_base.py
+++ anyblok/bloks/anyblok_core/tests/test_core_base.py
@@ -5,6 +5,8 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
+import pytest
+
 from anyblok.tests.testcase import BlokTestCase
 from ..exceptions import CoreBaseException
 
@@ -28,3 +30,32 @@ class TestCoreBase(BlokTestCase):
         M = self.registry.System.Model
         M2 = self.registry.System.get_model('Model.System.Model')
         self.assertEqual(M, M2)
+
+
+# @pytest.mark.usefixtures('rollback_registry')
+# class TestCoreBaseScope:
+def test_to_primary_keys(rollback_registry):
+    registry = rollback_registry
+    with pytest.raises(CoreBaseException) as ex:
+        raise KeyError()
+        test = registry.System()
+        test.to_primary_keys()
+
+
+def test_from_primary_keys(rollback_registry):
+    registry = rollback_registry
+    with pytest.raises(CoreBaseException) as ex:
+        raise KeyError()
+        registry.System.from_primary_keys()
+
+# def test_get_primary_keys(self):
+#     registry = rollback_registry
+#     with pytest.raises(CoreBaseException) as ex:
+#         registry.System.get_primary_keys()
+#
+# def test_get_model(self):
+#     registry = rollback_registry
+#     m = registry.System.Model
+#     m2 = registry.System.get_model('Model.System.Model')
+#     assert m == m2
+#
--- anyblok/conftest.py
+++ anyblok/conftest.py
@@ -1,1 +1,55 @@
+from logging import getLogger
+
 import pytest
+import sqlalchemy
+from sqlalchemy import event
+
+from anyblok.config import Configuration
+from anyblok.registry import RegistryManager
+from anyblok.blok import BlokManager
+
+logger = getLogger(__name__)
+
+
+@pytest.fixture(scope='session')
+def init_session(request):
+    # Init registry
+    additional_setting = {'unittest': True}
+
+    if len(BlokManager.list()) == 0:
+        BlokManager.load()
+    registry = RegistryManager.get(Configuration.get('db_name'),
+                                   **additional_setting)
+    registry.commit()
+    # Add savepoint
+    registry.begin_nested()
+
+    @event.listens_for(registry.session, 'after_transaction_end')
+    def restart_savepoint(session, transaction):
+        if transaction.nested and not transaction._parent.nested:
+            session.expire_all()
+            session.begin_nested()
+
+    print('-- create registry')
+    yield registry
+
+    request.addfinalizer(registry.session.close)
+
+
+@pytest.fixture(scope='function')
+def rollback_registry(request, init_session):
+    registry = init_session
+
+    print('-- inside rollback registry')
+    yield registry
+
+    def clean_up():
+        try:
+            registry.System.Cache.invalidate_all()
+        except sqlalchemy.exc.InvalidRequestError:
+            logger.warning('Invalid request Error: while invalidating all caches after {}'
+                           .format(request.function.__name__))
+        finally:
+            registry.rollback()
+
+    request.addfinalizer(clean_up)
