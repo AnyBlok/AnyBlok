09f1793f74a8ac6b64c38eda78b09a7d888bd353
denis-vivies-home <denis-vivies@netcourrier.com>
2018-10-05 17:01:29 +0200

    add checks of bloks list types and installed

--- anyblok/tests/conftest.py
+++ anyblok/tests/conftest.py
@@ -1,3 +1,5 @@
+import logging
+
 import pytest
 from anyblok.blok import BlokManager
 from anyblok.registry import RegistryManager
@@ -5,31 +7,41 @@ from anyblok.config import Configuration
 from anyblok.environment import EnvironmentManager
 from copy import deepcopy
 
+logger = logging.getLogger(__name__)
+
 
 def init_registry_with_bloks(bloks, function, **kwargs):
     if bloks is None:
         bloks = []
-
     if isinstance(bloks, tuple):
         bloks = list(bloks)
+    if isinstance(bloks, str):
+        bloks = [bloks]
 
-    if 'anyblok-test' not in bloks:
-        bloks.append('anyblok-test')
+    anyblok_test_name = 'anyblok-test'
+    if anyblok_test_name not in bloks:
+        bloks.append(anyblok_test_name)
 
     loaded_bloks = deepcopy(RegistryManager.loaded_bloks)
     if function is not None:
-        EnvironmentManager.set('current_blok', 'anyblok-test')
+        EnvironmentManager.set('current_blok', anyblok_test_name)
         try:
             function(**kwargs)
         finally:
             EnvironmentManager.set('current_blok', None)
-
     try:
         registry = RegistryManager.get(
             Configuration.get('db_manager'),
             unittest=False)
+
+        # update required blok
+        registry_bloks = registry.get_bloks_by_states('installed', 'toinstall')
         if bloks:
-            registry.upgrade(install=bloks)
+            for blok_to_install in bloks:
+                if blok_to_install not in registry_bloks:
+                    registry.upgrade(install=[blok_to_install])
+                else:
+                    registry.upgrade(update=[blok_to_install])
     finally:
         RegistryManager.loaded_bloks = loaded_bloks
 
