224f13283411568c35da9f9e9ec625066256a64e
jssuzanne <jssuzanne@anybox.fr>
2018-10-07 08:27:23 +0200

    Refactored test_authorization

--- anyblok/tests/test_blok_manager.py
+++ anyblok/tests/test_blok_manager.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # This file is a part of the AnyBlok project
 #
-#    Copyright (C) 2014 Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
+#    Copyright (C) 2018 Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
@@ -15,8 +15,7 @@ class TestBlokManager:
     @pytest.fixture(autouse=True)
     def bloks_must_be_unloaded(self, request):
         request.addfinalizer(BlokManager.unload)
-        if BlokManager.list():
-            BlokManager.unload()
+        BlokManager.unload()
 
     def test_load_anyblok(self):
         BlokManager.load()
