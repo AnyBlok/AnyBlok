74a7097351fc9e9304545e47b0e01f4042c6cbc4
jssuzanne <js.suzanne@gmail.com>
2019-05-13 20:10:14 +0200

    Flake8

--- anyblok/model/table_and_mapper.py
+++ anyblok/model/table_and_mapper.py
@@ -74,50 +74,63 @@ class TableMapperPlugin(ModelPluginBase):
         """
         table_args = tuple(properties['add_in_table_args'])
         if table_args:
-            def define_table_args(cls_):
-                if cls_.__registry_name__ == namespace:
-                    res = super(new_base, cls_).define_table_args()
-                    fks = [x.name for x in res
-                           if isinstance(x, ForeignKeyConstraint)]
-
-                    t_args = []
-                    for field in table_args:
-                        for constraint in field.update_table_args(cls_):
-                            if (
-                                not isinstance(constraint,
-                                               ForeignKeyConstraint) or
-                                constraint.name not in fks
-                            ):
-                                t_args.append(constraint)
-                            elif isinstance(constraint, CheckConstraint):
-                                t_args.append(constraint)
-
-                    return res + tuple(t_args)
-
-                return ()
-
-            new_base.define_table_args = classmethod(define_table_args)
+            new_base.define_table_args = self.define_table_args(
+                new_base, namespace, table_args)
             transformation_properties['table_args'] = True
 
         if self.registry.engine.url.drivername.startswith('mysql'):
-            def define_table_kwargs(cls_):
-                res = {}
-                if cls_.__registry_name__ == namespace:
-                    res = super(new_base, cls_).define_table_kwargs()
-
-                res.update(dict(
-                    mysql_engine='InnoDB',
-                    # mysql_charset='utf8mb4',
-                    # mysql_key_block_size="1024"
-                ))
-
-                return res
-
-            new_base.define_table_kwargs = classmethod(define_table_kwargs)
+            new_base.define_table_kwargs = self.define_table_kwargs(
+                new_base, namespace)
             transformation_properties['table_kwargs'] = True
 
         self.insert_in_bases_table_args(new_base, transformation_properties)
         self.insert_in_bases_mapper_args(new_base, transformation_properties)
+
+    def define_table_args(self, new_base, namespace, table_args):
+        """
+        :param new_base: the base to be put on front of all bases
+        :param namespace: the namespace of the model
+        """
+
+        def fnct(cls_):
+            if cls_.__registry_name__ == namespace:
+                res = super(new_base, cls_).define_table_args()
+                fks = [x.name for x in res
+                       if isinstance(x, ForeignKeyConstraint)]
+
+                t_args = []
+                for field in table_args:
+                    for constraint in field.update_table_args(cls_):
+                        if (
+                            not isinstance(constraint,
+                                           ForeignKeyConstraint) or
+                            constraint.name not in fks
+                        ):
+                            t_args.append(constraint)
+                        elif isinstance(constraint, CheckConstraint):
+                            t_args.append(constraint)
+
+                return res + tuple(t_args)
+
+            return ()
+
+        return classmethod(fnct)
+
+    def define_table_kwargs(self, new_base, namespace):
+        """
+        :param new_base: the base to be put on front of all bases
+        :param namespace: the namespace of the model
+        """
+
+        def fnct(cls_):
+            res = {}
+            if cls_.__registry_name__ == namespace:
+                res = super(new_base, cls_).define_table_kwargs()
+
+            res.update(dict(mysql_engine='InnoDB'))
+            return res
+
+        return classmethod(fnct)
 
     def insert_in_bases_table_args(self, new_base, transformation_properties):
         """Add table __table_args__ in new_base
