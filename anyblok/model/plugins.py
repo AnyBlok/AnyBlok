59253596ed9ee8f36c88a584271e85baeb396db2
jssuzanne <js.suzanne@gmail.com>
2019-09-25 09:31:14 +0200

    Speed up the build of the Model

--- anyblok/model/plugins.py
+++ anyblok/model/plugins.py
@@ -1,16 +1,19 @@
 # This file is a part of the AnyBlok project
 #
 #    Copyright (C) 2017 Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
+#    Copyright (C) 2019 Jean-Sebastien SUZANNE <js.suzanne@gmail.com>
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
+from functools import lru_cache
 from pkg_resources import iter_entry_points
 from logging import getLogger
 
 logger = getLogger(__name__)
 
 
+@lru_cache()
 def get_model_plugins(registry):
     res = []
     for i in iter_entry_points('anyblok.model.plugin'):
