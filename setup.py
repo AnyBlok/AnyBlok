40e4198af8b4f07c0afd0dfbd8bbc45564b64c35
jssuzanne <js.suzanne@gmail.com>
2020-08-27 11:13:27 +0200

    Force pymssql version in travis

--- setup.py
+++ setup.py
@@ -79,10 +79,10 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development',
