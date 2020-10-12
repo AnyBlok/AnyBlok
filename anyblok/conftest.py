83178ea5ceaae6b576054258c28b9a82adb6c437
Pierre Verkest <pierreverkest84@gmail.com>
2020-10-12 15:43:55 +0200

    provide skip_while_demo_data_installed marker

--- anyblok/conftest.py
+++ anyblok/conftest.py
@@ -62,15 +62,34 @@ def rollback_registry(request, init_session):
     def test_somthing(rollback_registry):
         registry = rollback_registry
         ...
+
+    If you want to skip test if demo data are installed you can
+    place ``skip_while_demo_data_installed`` pytest marker::
+
+    @pytest.mark.skip_while_demo_data_installed
+    def test_somthing(rollback_registry):
+        registry = rollback_registry
+        ...
     """
     registry = init_session
 
-    if request.node.get_closest_marker('skip_unless_demo_data_installed'):
-        if not registry.System.Parameter.get("with-demo", False):
-            pytest.skip(
-                "Demo data are required (Use ``--with-demo`` to create the "
-                "test database)."
-            )
+    if (
+        request.node.get_closest_marker('skip_unless_demo_data_installed') and
+        not registry.System.Parameter.get("with-demo", False)
+    ):
+        pytest.skip(
+            "Demo data are required (Use ``--with-demo`` to create the "
+            "test database)."
+        )
+
+    if (
+        request.node.get_closest_marker('skip_while_demo_data_installed') and
+        registry.System.Parameter.get("with-demo", False)
+    ):
+        pytest.skip(
+            "Demo data are present (Do not use ``--with-demo`` to create the "
+            "test database)."
+        )
 
     yield registry
 
