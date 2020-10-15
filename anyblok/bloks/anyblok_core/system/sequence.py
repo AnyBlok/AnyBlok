80fc6d2591b9be42429764b97317798a6f89720c
jssuzanne <js.suzanne@gmail.com>
2020-10-15 09:20:13 +0200

    Fixed flake8

--- anyblok/bloks/anyblok_core/system/sequence.py
+++ anyblok/bloks/anyblok_core/system/sequence.py
@@ -161,7 +161,7 @@ class Sequence:
         """
         if self.no_gap:
             self.refresh(with_for_update={"nowait": True})
-            nextval = self.current + 1 if self.current is not None else self.start
+            nextval = self.start if self.current is None else self.current + 1
         else:
             nextval = self.registry.execute(SQLASequence(self.seq_name))
         self.update(current=nextval)
