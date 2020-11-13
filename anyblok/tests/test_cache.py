85cba999b819e603012eba57466cd87f9f9f57ab
Pierre Verkest <pierreverkest84@gmail.com>
2020-11-13 15:20:11 +0100

    add unitest testing Cache.detect_invalidation on Fresh instance with no cache

--- anyblok/tests/test_cache.py
+++ anyblok/tests/test_cache.py
@@ -85,6 +85,12 @@ class TestCache:
         Cache.insert(registry_name="Model.Test", method="method_cached")
         assert Cache.detect_invalidation()
 
+    def test_detect_cache_invalidation_first_call(self, registry_method_cached):
+        registry = registry_method_cached
+        Cache = registry.System.Cache
+        Cache.last_cache_id = None
+        assert Cache.detect_invalidation()
+
     def test_get_invalidation(self, registry_method_cached):
         registry = registry_method_cached
         Cache = registry.System.Cache
