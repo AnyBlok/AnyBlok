9830d5465b5051981de8feb4676740794cd326b8
jssuzanne <js.suzanne@gmail.com>
2021-04-12 15:12:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if sys.version_info < (3, 5):
 
 requires = [
     'sqlalchemy >= 1.4.0',
-    'sqlalchemy-utils >= 0.33.0',
+    'sqlalchemy-utils >= 0.37.0',
     'sqlalchemy-views >= 0.3.0',
     'packaging',
     'setuptools',
