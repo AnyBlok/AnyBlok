2ccd5d2f3e259f84971a65bb021de0e7913f7195
jssuzanne <js.suzanne@gmail.com>
2023-02-17 02:20:17 +0100

    Fixed polymorphism on hybrid_property and constraint on hybrid property

---
+++ anyblok/schema.py
@@ -0,0 +1,75 @@
+from sqlalchemy.schema import (
+    ForeignKeyConstraint as SQLAForeignKeyConstraint,
+    UniqueConstraint as SQLAUniqueConstraint,
+    PrimaryKeyConstraint as SQLAPrimaryKeyConstraint,
+    CheckConstraint,  # noqa
+    Index as SQLAIndex,
+)
+
+
+class ForeignKeyConstraint(SQLAForeignKeyConstraint):
+
+    def __init__(self, columns, refcolumns, *args, **kwargs):
+        super().__init__(
+            [
+                (
+                    col.descriptor.sqla_column
+                    if hasattr(col.descriptor, 'sqla_column')
+                    else col
+                )
+                for col in columns
+            ],
+            [
+                (
+                    col.descriptor.sqla_column
+                    if hasattr(col.descriptor, 'sqla_column')
+                    else col
+                )
+                for col in refcolumns
+            ],
+            *args, **kwargs)
+
+
+class UniqueConstraint(SQLAUniqueConstraint):
+
+    def __init__(self, *columns, **kwargs):
+        super().__init__(
+            [
+                (
+                    col.descriptor.sqla_column
+                    if hasattr(col.descriptor, 'sqla_column')
+                    else col
+                )
+                for col in columns
+            ],
+            **kwargs)
+
+
+class PrimaryKeyConstraint(SQLAPrimaryKeyConstraint):
+
+    def __init__(self, *columns, **kwargs):
+        super().__init__(
+            [
+                (
+                    col.descriptor.sqla_column
+                    if hasattr(col.descriptor, 'sqla_column')
+                    else col
+                )
+                for col in columns
+            ],
+            **kwargs)
+
+
+class Index(SQLAIndex):
+
+    def __init__(self, *columns, **kwargs):
+        super().__init__(
+            [
+                (
+                    col.descriptor.sqla_column
+                    if hasattr(col.descriptor, 'sqla_column')
+                    else col
+                )
+                for col in columns
+            ],
+            **kwargs)
